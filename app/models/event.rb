9ca59bb717ce1850784e388de4cfa9b8d2e3b6c7
Fenix Wang <fenix.wang@ninjageek.ai>
2024-09-04 22:41:12 +0800

    练习ORM关系实体

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
+  has_many :event_groupships
+  has_many :groups, through: :event_groupships
+  has_one :location, dependent: :destroy
+  belongs_to :category, optional: true
+  has_many :attendees, -> { where([ "created_at > ?", Time.now - 7.day ]).order("id DESC") }, dependent: :destroy
   validates_presence_of :name
 end
